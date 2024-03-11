39e10e632a2e809abd0c2b1b296831d5baeee47f
Marynka-a <152696353+Marynka-a@users.noreply.github.com>
2024-03-11 16:22:28 +0200

    Create service_creation _in_Windows.c

---
+++ с/service_creation _in_Windows.c
@@ -0,0 +1,49 @@
+#Count the number of even numbers among those integers entered by the user.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+// A function that counts the number of even numbers
+int countEvenNumbers(int *numbers, int size) {
+    int count = 0;
+    for (int i = 0; i < size; ++i) {
+        if (numbers[i] % 2 == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+// A function that performs calculations and displays the result
+void calculateAndPrint(int *numbers, int size) {
+    int evenCount = countEvenNumbers(numbers, size);
+    printf("Кількість парних чисел: %d\n", evenCount);
+}
+
+// Main function
+int main() 
+{
+    SetConsoleOutputCP(CP_UTF8);
+    int size;
+    printf("Enter the number of integers: ");
+    scanf("%d", &size);
+
+    int *numbers = (int *)malloc(size * sizeof(int));
+    if (numbers == NULL) 
+    {
+        printf("Error allocating memory\n");
+        return 1;
+    }
+
+    printf("Enter integers separated by a space: ");
+    for (int i = 0; i < size; ++i) 
+    {
+        scanf("%d", &numbers[i]);
+    }
+
+    calculateAndPrint(numbers, size);
+
+    free(numbers);
+    return 0;
+}
